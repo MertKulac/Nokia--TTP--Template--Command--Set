1057c8b0b892d442a562f76301d127dd3e523c29
Mert Kulaç <96883175+MertKulac@users.noreply.github.com>
2022-02-13 16:27:24 +0300

    Create show router ospf neighbor.py

---
+++ show router ospf neighbor.py
@@ -0,0 +1,48 @@
+from pprint import pprint
+from ttp import ttp
+import json
+import time
+
+with open("showRouterOspfNeighbor.txt") as f:
+   data_to_parse = f.read()
+
+ttp_template = """
+<group name="Show_Router_Ospf_Neighbor">
+{{Interface-Name}}                          {{Rtr_Id}}      {{Neighbor_State}}       {{Pri|DIGIT}}    {{RetxQ}}       {{TTL|DIGIT}}
+   {{Area_ID}}
+</group>
+"""
+
+parser = ttp(data=data_to_parse, template=ttp_template)
+parser.parse()
+
+# print result in JSON format
+results = parser.result(format='json')[0]
+print(results)
+
+***RESULT***
+[appadmin@ryugbz01 Nokia]$ python3 showRouterOspfNeighbor.py
+[
+    {
+        "Show_Router_Ospf_Neighbor": [
+            {
+                "Area_ID": "0.0.0.0",
+                "Interface-Name": "toSR7-3",
+                "Neighbor_State": "Full",
+                "Pri": "1",
+                "RetxQ": "0",
+                "Rtr_Id": "10.10.10.3",
+                "TTL": "33"
+            },
+            {
+                "Area_ID": "1.1.1.1",
+                "Interface-Name": "toSR7-4",
+                "Neighbor_State": "Full",
+                "Pri": "10",
+                "RetxQ": "0",
+                "Rtr_Id": "10.10.12.3",
+                "TTL": "34"
+            }
+        ]
+    }
+]
